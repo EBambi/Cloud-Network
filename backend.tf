6249e6ac367eb972ecb25c88e9e4e2ca471fb84f
Esteban <estebanyalg@gmail.com>
2024-01-17 10:53:11 -0500

    Fix region from west to east

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-sandbox.network"
     key            = "aws/sandbox/Peex/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-2"
     dynamodb_table = "terraform-sandbox-us-west-2-lock-dynamo"
     encrypt        = true
   }
