31d07c6273057b1ce088891970822958f6c41a40
Esteban <estebanyalg@gmail.com>
2024-01-17 11:02:17 -0500

    Roll back terraform.tfstate

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-sandbox.network"
-    key            = "aws/sandbox/Peex/"
+    key            = "aws/sandbox/Peex/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-sandbox-us-west-2-lock-dynamo"
     encrypt        = true
