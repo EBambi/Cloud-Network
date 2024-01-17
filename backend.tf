8c7dde0e03158e79021d9cfdb71c7a89e42da03c
Esteban <estebanyalg@gmail.com>
2024-01-17 10:57:27 -0500

    Push actual terraform state to S3 bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-sandbox.network"
-    key            = "aws/sandbox/Peex/terraform.tfstate"
+    key            = "aws/sandbox/Peex/"
     region         = "us-east-2"
     dynamodb_table = "terraform-sandbox-us-west-2-lock-dynamo"
     encrypt        = true
