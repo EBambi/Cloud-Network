c66375c1e86299006dd050bbd1c4ac1e081633ee
Esteban <estebanyalg@gmail.com>
2024-01-17 14:02:23 -0500

    Add new dynamoDB table and change order

--- backend.tf
+++ backend.tf
@@ -1,3 +1,14 @@
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket         = "terraform-sandbox.network"
@@ -12,13 +23,3 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "1106"
-    type = "N"
-  }
-}
