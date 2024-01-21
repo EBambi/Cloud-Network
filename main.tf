bbf775b0a3c0782dfe9c893b14a6cd02a2fcc9cb
Esteban <estebanyalg@gmail.com>
2024-01-21 09:27:17 -0500

    Change VPC creation order

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-resource "aws_instance" "ubuntu" {
-    ami                     = var.instance_ami_ubuntu
-    instance_type           = var.instance_type
-    security_groups         = ["esteban-sandbox-group"]
-    key_name                = "deployer-key"
-    vpc_security_group_ids  = ["vpc_us-west-2_esteban"]
-
-    tags = {
-        Name    = "ubuntu-esteban"
-    }
-}
-
 resource "aws_security_group" "allow_local" {
     name        = "esteban-lab-group"
     description = "Allow all inbound local traffic"
@@ -30,11 +18,23 @@ resource "aws_security_group" "allow_local" {
     }
 }
 
+resource "aws_vpc" "vpc_us-west-2_esteban" {
+  cidr_block    = "192.168.0.0/16"
+}
+
 resource "aws_key_pair" "deployer" {
     key_name    = "deployer-key"
     public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4ojoTfOUYpSgPzcC8SfTnMslu9j08CPzWpGDDz7FwcFbAALZShRnpQ6R+lDew2vqts+ipJj7MBwjwIRfPRYRQ9Gw6VCRbHRmzzUJkyiH4c9SG4AfGboRKgSAJJq3+jPqr9OMARLNhAURLygJ5UJ6m65CFjjnYy2BAylRyETzLlgPnTJM5KRz+26gswTdp3I30xcGrEj1jcFVRqR19XNXIt4UprOOgeehByPSpjdy+3KnBE6yiiKN+dbt0lUfD5x6jQwI4Df2X8HL63Lj/yePUVmwPFL0WdN2AJFjRpBMo2sVowVBmaZo10tse6sN1wsoj6b9iMWqJ154GOI7RExqNLJEaLVi1BWW34hY2YoRdGQ7QAYZ8OoQ5jwAKCJkoAKs9PRjakw7ugLNrYsbIXul33v/z8vJFkxRRgvCFqP7beM7U/WzEBkK4xG22qVzQC9xTPoU8VQeW5P53KGy6IYSx4Gutx4cShw7Vj2bn6IG5yVvpJpzCSlMBdrb1U+DMbcBPteKjZbrSxfHEd52BmamLQ1dQPBbJS2JvvZwamvVAIMzwuUhhrJ9DQurY40fd6qg+pTIAq5pjSM3qTW/V+OZaTydCrGMOYW3BHlhEav4PiPa2caUcBvYU3cQ6ojlEnNrDtrDeewDVpQF4Obvjul+rtmxCpUGYAh0PeEx5dBtR1Q== estebago@ESTEBAGO-M-X9Q9"
 }
 
-resource "aws_vpc" "vpc_us-west-2_esteban" {
-  cidr_block    = "192.168.0.0/16"
+resource "aws_instance" "ubuntu" {
+    ami                     = var.instance_ami_ubuntu
+    instance_type           = var.instance_type
+    security_groups         = ["esteban-sandbox-group"]
+    key_name                = "deployer-key"
+    vpc_security_group_ids  = ["vpc_us-west-2_esteban"]
+
+    tags = {
+        Name    = "ubuntu-esteban"
+    }
 }
