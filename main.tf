7f1addd089541f6be4bd6dbdc59a523d03f9e5cf
Esteban <estebanyalg@gmail.com>
2024-01-21 09:29:11 -0500

    Change resource ubuntu to ubuntu-ec2

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_key_pair" "deployer" {
     public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC4ojoTfOUYpSgPzcC8SfTnMslu9j08CPzWpGDDz7FwcFbAALZShRnpQ6R+lDew2vqts+ipJj7MBwjwIRfPRYRQ9Gw6VCRbHRmzzUJkyiH4c9SG4AfGboRKgSAJJq3+jPqr9OMARLNhAURLygJ5UJ6m65CFjjnYy2BAylRyETzLlgPnTJM5KRz+26gswTdp3I30xcGrEj1jcFVRqR19XNXIt4UprOOgeehByPSpjdy+3KnBE6yiiKN+dbt0lUfD5x6jQwI4Df2X8HL63Lj/yePUVmwPFL0WdN2AJFjRpBMo2sVowVBmaZo10tse6sN1wsoj6b9iMWqJ154GOI7RExqNLJEaLVi1BWW34hY2YoRdGQ7QAYZ8OoQ5jwAKCJkoAKs9PRjakw7ugLNrYsbIXul33v/z8vJFkxRRgvCFqP7beM7U/WzEBkK4xG22qVzQC9xTPoU8VQeW5P53KGy6IYSx4Gutx4cShw7Vj2bn6IG5yVvpJpzCSlMBdrb1U+DMbcBPteKjZbrSxfHEd52BmamLQ1dQPBbJS2JvvZwamvVAIMzwuUhhrJ9DQurY40fd6qg+pTIAq5pjSM3qTW/V+OZaTydCrGMOYW3BHlhEav4PiPa2caUcBvYU3cQ6ojlEnNrDtrDeewDVpQF4Obvjul+rtmxCpUGYAh0PeEx5dBtR1Q== estebago@ESTEBAGO-M-X9Q9"
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu-ec2" {
     ami                     = var.instance_ami_ubuntu
     instance_type           = var.instance_type
     security_groups         = ["esteban-sandbox-group"]
